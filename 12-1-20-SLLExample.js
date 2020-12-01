daf74f207bf9094eb63818c680d5f5e77ed07d9c
Daniel Nguyen <theonedanielnguyen@gmail.com>
2020-12-01 10:13:10 -0800

    First Commit

---
+++ 12-1-20-SLLExample.js
@@ -0,0 +1,37 @@
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    // return true or false if this.head is null
+    isEmpty() {
+        if (this.head) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // add given node to the head, if it exists. return void
+    addToFront(node) {
+        node.next = this.head; // set the new node's next to the head
+        this.head = node; // move the head to the new node
+    }
+
+    // createa  new node with given data, add it to the head. return void
+    addDataToFront(data) {
+        var newNode = new Node(data); // create a new node with the data
+        newNode.next = this.head; // set the new node's next to the head
+        this.head = node; // move the head to the new node
+    }
+}
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+var node = new Node(10);
+var mySLL = new SLL();
