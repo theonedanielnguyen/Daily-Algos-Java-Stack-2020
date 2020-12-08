e7ab3622a11b9be03a6ac368d51b3195b2c1af90
Daniel Nguyen <theonedanielnguyen@gmail.com>
2020-12-07 19:21:48 -0800

    Daily algo upload

---
+++ 12-7-20-StackExample.js
@@ -0,0 +1,122 @@
+// Stacks
+
+// A stack is a LIFO data structure
+// LAST IN, FIRST OUT
+// LIFO
+
+// push - add to top
+// pop - remove from top
+// peek - check the top
+// isEmpty - check if the stack is empty, true/false
+// length - return size of stack
+
+
+
+class arrStack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(item) {
+        this.items.push(item);
+    }
+
+    pop() {
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        if (this.items.length === 0) {
+            return true;
+        }
+        return false;
+    }
+
+    length() {
+        return this.items.length;
+    }
+}
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class slStack {
+    constructor() {
+        this.top = null; // this.head, this.end
+        this.length = 0;
+    }
+
+    // add to top
+    push(newNode) {
+        if (this.top === null) {
+            this.top = newNode;
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
+        }
+        this.length++;
+    }
+
+    // remove from top
+    pop() {
+        if (this.top === null) return null;
+
+        const removed = this.top; // store previous top
+        this.top = this.top.next; // move top pointer
+        removed.next = null; // remove pointer from stored node
+        this.length--; // decrement length
+
+        return removed; // return the node
+    }
+
+    // Node myNode = mySLL.pop();
+
+    // aka check top
+    peek() {
+        return this.top;
+    }
+
+    // check if empty
+    isEmpty() {
+        return this.top === null;
+    }
+
+    // length getter
+    length() {
+        return this.length;
+    }
+}
+
+// buh buh bonus challenge: countStack
+
+// write the standalone function countStack
+// given a slStack, count the nodes
+// return the count
+// you may use one stack or array as additional storage
+// the given stack must be returned back to it's original order
+// you may only use public stack methods push pop peek isempty
+function countStack(stack) {
+    let newStack = new slStack();
+
+    let count = 0;
+
+    while (!stack.isEmpty()) {
+        let node = stack.pop();
+        newStack.push(node);
+        count++;
+    }
+
+    while (!newStack.isEmpty()) {
+        stack.push(newStack.pop());
+    }
+
+    return count;
+};
